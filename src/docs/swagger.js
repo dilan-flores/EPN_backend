faf82d9f2e66883bdb7f1c0d12cf10151d97ba94
dilan-flores <dilan.flores@epn.edu.ec>
2024-01-14 18:31:34 -0500

    Swagger URL

--- src/docs/swagger.js
+++ src/docs/swagger.js
@@ -17,7 +17,7 @@ const swaggerOptions = {
         },
         servers: [
             {
-                url: "http://localhost:3000"
+                url: "https://epn-backend.onrender.com"
             }
         ],
     },
